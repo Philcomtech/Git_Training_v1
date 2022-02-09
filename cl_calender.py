dfa495d27ad9b8cffd084548cffd6e0bbc8c5eba
Nwamina Philip <nwamina.philip@gmail.com>
2022-02-09 13:23:45 -0600

    Updated to Python v3

--- cl_calender.py
+++ cl_calender.py
@@ -2,40 +2,40 @@
 This is a Command Line Calender a little modification
 """
 from time import sleep, strftime
-User_Name = raw_input("What is your name please: ")
+User_Name = input("What is your name please: ")
 calender = {}
 def welcome():
-  print "Welcome, " + User_Name + "."
-  print "Calender starting..."
+  print ("Welcome, " + User_Name + ".")
+  print ("Calender starting...")
   sleep(1)
-  print "Today is: " + strftime("%A %B %d, %Y")
-  print "Time is: " + strftime("%I") + ":"+ strftime("%M") + ":" + strftime("%S") 
+  print ("Today is: " + strftime("%A %B %d, %Y"))
+  print ("Time is: " + strftime("%I") + ":"+ strftime("%M") + ":" + strftime("%S"))
   sleep(1)
-  print "What would you like to do?"
+  print ("What would you like to do?")
 
 def start_calender():
   welcome()
   start = True
   while start:
-    user_choice = raw_input("Enter A to Add, U to Update, V to View, D to delete, X to Exit: ")
+    user_choice = input("Enter A to Add, U to Update, V to View, D to delete, X to Exit: ")
     user_choice = user_choice.upper()
     if user_choice == "V":
       if len(calender.keys()) < 1:
-        print "Calender empty."
+        print ("Calender empty.")
       else:
-        print calender
+        print (calender)
     elif user_choice == "U":
-      date = raw_input("What date? ")
-      update = raw_input("Enter the update: ")
+      date = input("What date? ")
+      update = input("Enter the update: ")
       calender[date] = update
-      print "Update Successful"
-      print calender
+      print ("Update Successful")
+      print (calender)
     elif user_choice == "A":
-      event = raw_input("Enter event: ")
-      date = raw_input("Enter date (MM/DD/YYYY): ")
+      event = input("Enter event: ")
+      date = input("Enter date (MM/DD/YYYY): ")
       if (len(date) > 10 or int(date[6:]) < int(strftime("%Y"))):
-        print "invalid date" 
-        try_again = raw_input("Try Again? Y for Yes, N for No: ")
+        print ("invalid date") 
+        try_again = input("Try Again? Y for Yes, N for No: ")
         try_again = try_again.upper()
         if try_again == "Y":
           continue
@@ -43,26 +43,24 @@ def start_calender():
           start = False
       else:
         calender [date] = event
-        print "Event Successfully Added"
-        print calender
+        print ("Event Successfully Added")
+        print (calender)
     elif user_choice == "D":
       if (len(calender.keys()) < 1):
-        print "calender is empty"
+        print ("calender is empty")
       else:
-        event = raw_input("What event? ")
+        event = input("What event? ")
         for date in calender.keys():
           if event == calender[date]:
             del calender[date]
-            print "Event Successfully Deleted"
+            print ("Event Successfully Deleted")
           else:
-            print "Incorrect event was specified"
+            print ("Incorrect event was specified")
     elif user_choice == "X":
       start = False
     else:
-      print "Invalid Entry"
+      print ("Invalid Entry")
       start = False
 
 start_calender()
 
-
-SAMPLE
